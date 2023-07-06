89f9b212491114dd8e4c8d8573a1da12f4e2da81
athulcoder <athulsabu324@gmail.com>
2023-07-06 17:27:06 +0530

    commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -5,6 +5,8 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     cv2.imshow('Attendance AI', frame)
+
+    # Press q to Quit the Screen
     if cv2.waitKey(1)==ord('q'):
         break
 
